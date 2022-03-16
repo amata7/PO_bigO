0e360df33439fd76217c8673ba6f892db804029f
amata7 <adrianmata@live.com>
2022-03-16 14:12:48 -0700

    added console logs to show functionality when "node stackMethods.js" is run

--- stackMethods.js
+++ stackMethods.js
@@ -1,9 +1,10 @@
-let valArray = [1, 2, 3, 7, 1, 1, 6, 2];
+let valArray = [];
 
 class Stack {
   constructor(val) {
     this.val = val;
   }
+  static valArray = [];
   pushToStack(val) {
     if (val === undefined) {
       throw new Error('Missing parameter to push');
@@ -13,14 +14,14 @@ class Stack {
   }
   popFromStack() {
     if (valArray.length === 0) {
-      throw new Error('No values inside of array.');
+      throw new Error('Cannot pop a value from an empty array');
     } else {
       return valArray.pop();
     }
   }
   maxFromStack() {
     if (valArray.length === 0) {
-      throw new Error('No values inside of array.');
+      throw new Error('Cannot find maximum value from an empty array');
     } else {
       return valArray.reduce(function (a, b) {
         return Math.max(a, b);
@@ -28,3 +29,37 @@ class Stack {
     }
   }
 }
+
+const test = new Stack();
+console.log({ 'Initial Array': valArray });
+console.log({ 'Testing push': test.pushToStack(1) }, { 'New Array': valArray });
+console.log({ 'Testing push': test.pushToStack(2) }, { 'New Array': valArray });
+console.log({ 'Testing push': test.pushToStack(7) }, { 'New Array': valArray });
+console.log({ 'Testing max': test.maxFromStack() });
+console.log(
+  { 'Testing push': test.pushToStack(457) },
+  { 'New Array': valArray }
+);
+console.log({ 'Testing max': test.maxFromStack() });
+console.log(
+  { 'Testing pop': test.popFromStack() },
+  { 'Popped Array': valArray }
+);
+console.log({ 'Testing max': test.maxFromStack() });
+console.log(
+  { 'Testing pop': test.popFromStack() },
+  { 'Popped Array': valArray }
+);
+console.log({ 'Testing max': test.maxFromStack() });
+console.log(
+  { 'Testing pop': test.popFromStack() },
+  { 'Popped Array': valArray }
+);
+console.log({ 'Testing max': test.maxFromStack() });
+console.log(
+  { 'Testing pop': test.popFromStack() },
+  { 'Popped Array': valArray }
+);
+// Uncomment below console logs to test error handling of the max or pop functions one at a time
+// console.log({ 'Testing max': test.maxFromStack() });
+// console.log({ 'Testing pop': test.popFromStack() }, { 'Popped Array': valArray });
