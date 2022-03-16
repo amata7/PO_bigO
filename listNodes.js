5fd9679e2a36f776f4a6c17c4cccaa6646d36604
amata7 <adrianmata@live.com>
2022-03-16 14:45:45 -0700

    Linked List and Node classes created, test lists created

--- listNodes.js
+++ listNodes.js
@@ -1,9 +1,35 @@
-let listA = [1, 2, 3];
-let listB = [3];
-let addedLength = listA.length + listB.length;
-
-function findNode(addedLength) {
-  console.log(addedLength);
+class ListNode {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
 }
 
-findNode(addedLength);
+class LinkedList {
+  constructor(head = null) {
+    this.head = head;
+  }
+}
+
+let nodeA1 = new ListNode(3);
+let nodeA2 = new ListNode(7);
+nodeA1.next = nodeA2;
+let nodeA3 = new ListNode(8);
+nodeA2.next = nodeA3;
+let nodeA4 = new ListNode(10);
+nodeA3.next = nodeA4;
+let listA = new LinkedList(nodeA1);
+
+let nodeB1 = new ListNode(99);
+let nodeB2 = new ListNode(1);
+nodeB1.next = nodeB2;
+let nodeB3 = new ListNode(8);
+nodeB2.next = nodeB3;
+let nodeB4 = new ListNode(10);
+nodeB3.next = nodeB4;
+let listB = new LinkedList(nodeB1);
+
+console.log(listA);
+console.log({ 'Testing A': listA.head.next.next.data });
+console.log(listB);
+console.log({ 'Testing B': listB.head.next.next.data });
