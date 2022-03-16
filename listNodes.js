1382161eb11ffb2456bb8581051a44fb1be83891
amata7 <adrianmata@live.com>
2022-03-16 13:16:21 -0700

    file created, function console logs length of 2 lists added together

---
+++ listNodes.js
@@ -0,0 +1,9 @@
+let listA = [1, 2, 3];
+let listB = [3];
+let addedLength = listA.length + listB.length;
+
+function findNode(addedLength) {
+  console.log(addedLength);
+}
+
+findNode(addedLength);
