d03c4ca77abadd179da01e4e345dbefb235a086c
amata7 <adrianmata@live.com>
2022-03-17 21:02:28 -0700

    function returns the value where nodes intersect, example created and commented out

--- listNodes.js
+++ listNodes.js
@@ -20,30 +20,93 @@ class LinkedList {
   }
 }
 
-let nodeA1 = new ListNode(3);
-let nodeA2 = new ListNode(7);
-let nodeA3 = new ListNode(8);
-let nodeA4 = new ListNode(10);
-nodeA1.next = nodeA2;
-nodeA2.next = nodeA3;
-nodeA3.next = nodeA4;
-
-let nodeB1 = new ListNode(99);
-let nodeB2 = new ListNode(1);
-let nodeB3 = new ListNode(8);
-let nodeB4 = new ListNode(10);
-nodeB1.next = nodeB2;
-nodeB2.next = nodeB3;
-nodeB3.next = nodeB4;
-
-let listA = new LinkedList(nodeA1);
-let listB = new LinkedList(nodeB1);
-let combinedSize = listA.size() + listB.size();
-
-function findIntersectingNode(combinedSize) {
-  console.log({ A: listA });
-  console.log({ B: listB });
-  console.log(combinedSize);
+function findIntersectingNode(A, B) {
+  const arrA = [];
+  const arrB = [];
+  let headA = A.head;
+  let headB = B.head;
+  while (headA !== null) {
+    arrA.push(headA.data);
+    headA = headA.next;
+  }
+  while (headB !== null) {
+    arrB.push(headB.data);
+    headB = headB.next;
+  }
+  console.log({ 'First List': arrA });
+  console.log({ 'Second List': arrB });
+  const intersection = arrA.filter((element) => arrB.includes(element));
+  if (intersection.length === 0) {
+    console.log('There is no intersection between these two lists');
+  } else {
+    console.log(
+      'These two lists intersect when node value equals ' + intersection
+    );
+  }
 }
+//Uncomment the following line and use 'node listNodes.js' to run the example below
+// runTest();
+function runTest() {
+  let testA1 = new ListNode(3);
+  let testA2 = new ListNode(7);
+  let testA3 = new ListNode(8);
+  let testA4 = new ListNode(10);
+  testA1.next = testA2;
+  testA2.next = testA3;
+  testA3.next = testA4;
 
-findIntersectingNode(combinedSize);
+  let testB1 = new ListNode(99);
+  let testB2 = new ListNode(1);
+  let testB3 = new ListNode(8);
+  let testB4 = new ListNode(11);
+  testB1.next = testB2;
+  testB2.next = testB3;
+  testB3.next = testB4;
+
+  let listA = new LinkedList(testA1);
+  let listB = new LinkedList(testB1);
+  console.log('\n---Test using listA and listB---');
+  findIntersectingNode(listA, listB);
+
+  let testC1 = new ListNode(1);
+  let testC2 = new ListNode(2);
+  let testC3 = new ListNode(3);
+  let testC4 = new ListNode(4);
+  testC1.next = testC2;
+  testC2.next = testC3;
+  testC3.next = testC4;
+
+  let testD1 = new ListNode(3199);
+  let testD2 = new ListNode(561);
+  let testD3 = new ListNode(1);
+  let testD4 = new ListNode(1452);
+  testD1.next = testD2;
+  testD2.next = testD3;
+  testD3.next = testD4;
+
+  let listC = new LinkedList(testC1);
+  let listD = new LinkedList(testD1);
+  console.log('\n---Test using listC and listD---');
+  findIntersectingNode(listC, listD);
+
+  let testE1 = new ListNode(1);
+  let testE2 = new ListNode(2);
+  let testE3 = new ListNode(3);
+  let testE4 = new ListNode(4);
+  testE1.next = testE2;
+  testE2.next = testE3;
+  testE3.next = testE4;
+
+  let testF1 = new ListNode(5);
+  let testF2 = new ListNode(6);
+  let testF3 = new ListNode(7);
+  let testF4 = new ListNode(8);
+  testF1.next = testF2;
+  testF2.next = testF3;
+  testF3.next = testF4;
+
+  let listE = new LinkedList(testE1);
+  let listF = new LinkedList(testF1);
+  console.log('\n---Test using listE and listF---');
+  findIntersectingNode(listE, listF);
+}
